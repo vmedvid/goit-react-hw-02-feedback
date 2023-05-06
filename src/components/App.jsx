6cdbb595bff2c666a30f184edde1911e292b0a56
vmedvid <vmedvid22@gmail.com>
2023-05-06 16:20:52 +0300

    зробив завд.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100);
+  };
+
+  handleFeedbackBtn = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleFeedbackBtn}
+          ></FeedbackOptions>
+        </Section>
+        <Section title="Statistics">
+          {good || neutral || bad ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            ></Statistics>
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const FeedbackButton = styled.button`
+  margin: 10px;
+  padding: 5px 15px;
+  font-size: 20px;
+  font-weight: 500;
+  color: #000;
+  border-radius: 5px;
+  border: 1px solid #535353;
+  text-transform: capitalize;
+  cursor: pointer;
+`;
+
+export const FeedbackWrap = styled.div`
+  display: flex;
+  gap: 20px
+  margin: 10px 0;
+  padding: 10px;
+`;
